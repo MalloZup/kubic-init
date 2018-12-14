b511c92d066a09f8a6f46e654c283f139a50bfec
dmaiocchi <dmaiocchi@suse.com>
2018-12-14 15:37:19 +0100

    get IP from json and adapt pipeline to run e2e

--- build/make/runners.mk
+++ build/make/runners.mk
@@ -166,7 +166,12 @@ kubelet-reset: kubeadm-reset
 	$(SUDO) cp -f $(KUBELET_CONFIG) /var/lib/kubelet/config.yaml
 	@-rm -f $(MANIFEST_DIR)/$(MANIFEST_REM)
 
-
+#############################################################
+# End To End Tests
+#############################################################
+SEEDER := $(shell cd $(TF_LIBVIRT_FULL_DIR) && terraform output -json seeder 2>/dev/null | python -c 'import sys, json; print json.load(sys.stdin)["value"]' 2>/dev/null || echo "unknown")
+tf-e2e-tests:
+	cd tests && SEEDER=$(SEEDER) ./run_suites.sh
 #############################################################
 # Terraform deployments
 #############################################################
